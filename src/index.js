eded3bccc7fb7175be4cebebba55578581e599aa
casper992 <60609364+casper992@users.noreply.github.com>
2021-03-14 11:26:52 +0300

    docs: change file

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    if(array != undefined && array.length != 0) {
+        return Math.min(...array);
+    } else {
+        return 0;
+    }
 }
 
 exports.max = function max (array) {
-  return 0;
+    if(array != undefined && array.length != 0) {
+        return Math.max(...array);
+    } else {
+        return 0;
+    }
 }
 
+
 exports.avg = function avg (array) {
-  return 0;
+    if(array != undefined && array.length != 0) {
+        return array.reduce((acc, cur) => acc + cur, 0) / array.length;
+    } else {
+        return 0;
+    }
 }
+
+
+
